9163cf25ab8cb4fe67695f5015775bbdfc3a15c0
Parsakon Homvech <phomv001@ucr.edu>
2019-04-29 01:39:51 -0700

    Update clScraper.py

--- clScraper.py
+++ clScraper.py
@@ -19,9 +19,6 @@ class clSpyder(scrapy.Spider):
             yield {'Title': title}
         relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
         absolute_next_url = response.urljoin(relative_next_url)
-<<<<<<< HEAD
+
         yield Request(absolute_next_url, callback=self.parse)
-=======
-        
-        yield Request(absolute_next_url, callback=self.spyderparse)
->>>>>>> 3e90977f7be7aceaca753bc7a6bc1ba5f83598ca
+
