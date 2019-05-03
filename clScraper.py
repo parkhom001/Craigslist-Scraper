6054c1bc8ff03415d4ca072b6e2fcd511a4b0acd
Parsakon <phomv001@ucr.edu>
2019-05-03 04:10:44 -0700

    Goes into listing page and retrieves geolocation and description

--- clScraper.py
+++ clScraper.py
@@ -6,6 +6,7 @@ Created on Sat Apr 20 00:09:54 2019
 @author: parsakon
 """
 import scrapy
+
 from scrapy import Request
 
 class clSpyder(scrapy.Spider):
@@ -13,18 +14,31 @@ class clSpyder(scrapy.Spider):
     allowed_domains = ['craigslist.org']
     start_urls = ['https://losangeles.craigslist.org/d/real-estate/search/rea'] # pull data from this website
     
+    def parseListing(self, response):
+        url = response.meta.get('URL')
+        title = response.meta.get('Title')
+        price = response.meta.get('Price')
+        location = response.meta.get('Location')
+        date = response.meta.get('Date')
+        
+        description = "".join(line for line in response.xpath('//*[@id="postingbody"]/text()').extract())
+        geoposition = response.xpath('//head/meta[@name="geo.position"]/@content').get()
+        yield{'URL': url, 'Title': title, 'Description':description, 'Price':price, 'Location':location,'Date':date, 'Description':description,'Geoposition':geoposition}
+
     def parse(self, response):
         listings = response.xpath('//p[@class="result-info"]')
         for listing in listings:
             title = listing.xpath('a/text()').extract_first()
             price = listing.xpath('span/span[@class="result-price"]/text()').extract_first()
             location = listing.xpath('span/span[@class="result-hood"]/text()').extract_first()
-            yield {'Title': title,
-                   'Price': price,
-                   'Location': location
-                   }
+            relative_url = listing.xpath('a/@href').extract_first()
+            absolute_url = response.urljoin(relative_url)
+            date = listing.xpath('time/@title').extract_first()
+            
+            yield Request(absolute_url, callback=self.parseListing, meta={'URL':absolute_url,'Title':title,'Price':price,'Location':location,'Date':date})
 
         relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
         absolute_next_url = response.urljoin(relative_next_url)
 
         yield Request(absolute_next_url, callback=self.parse)
+    
